5483de8c1fd40fa64243cc4d63eb5f317cc211e2
HACKER ANANDH <48098789+anandh12469@users.noreply.github.com>
2021-09-09 13:58:44 +0530

    Update androdroid.py

--- androdroid.py
+++ androdroid.py
@@ -1,8 +1,10 @@
 import pyfiglet
 import os
 import socket
+from urllib.request import urlopen
+from sys import argv, exit
 
-ascii_banner = pyfiglet.figlet_format("ANDRO DROID")
+ascii_banner = pyfiglet.figlet_format("DOT DROID")
 print(ascii_banner)
 def create_payload(b, e):
     print(b)
@@ -21,7 +23,31 @@ def create_payload(b, e):
 def exploit():
     print("We are working on it")
 
-print("1. Create Payload \n2. Exploit Payload")
+def check(url):
+    print("IP Address: " + socket.gethostbyname(url))
+    try:
+        if "http" not in url: url = "http://" + url
+        data = urlopen(url)
+        headers = data.info()
+        if not "X-Frame-Options" in headers: return True
+    except: return False
+
+def create_poc(url):
+    code = """
+<html>
+   <head><title>Clickjack test page</title></head>
+   <body>
+     <p>Website is vulnerable to clickjacking!</p>
+     <iframe src="{}" width="500" height="500"></iframe>
+   </body>
+</html>
+    """.format(url)
+
+    with open(url + ".html", "w") as f:
+        f.write(code)
+        f.close()
+
+print("1. Create Payload \n2. Exploit Payload \n3. Click Jacking Test")
 a = int(input("Choose the Option: "))
 if a == 1:
     print("1. Choose automatically \n2. I given IP Adrress")
@@ -38,3 +64,17 @@ if a == 1:
 
 if a == 2:
     exploit()
+
+if a == 3:
+    url = input("Please Enter URL: ")
+    status = check(url)
+    if status:
+        print("Website is vulnerable!")
+        g = input("Are you need to generate payload(y/n): ")
+        if g == "y":
+            create_poc(url)
+            print("PoC stored in your work directory")
+        else:
+            print("Thanks For Using our script")
+    else:
+        print("Website is not vulnerable!")
